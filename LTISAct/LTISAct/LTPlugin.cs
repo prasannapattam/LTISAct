e4da36b65c120b4df1bb98471946ac581124078a
Prasanna Pattam <pprasannak@hotmail.com>
2014-08-15 01:15:26 -0400

    Changes

--- LTISAct/LTISAct/LTPlugin.cs
+++ LTISAct/LTISAct/LTPlugin.cs
@@ -16,7 +16,7 @@ namespace LTISAct
         {
             string currentUserID = session.Framework.CurrentUser.ID.ToString();
             string content = @"<input type=""hidden"" id=""LTCurrentUserID"" value=""" + currentUserID + @"""><span id=""spnCount"" style=""color: red; font-size: 12pt; font-weight: bold; text-decoration: underline;""></span>";
-            session.Menu.Items.Add("LTContacts", content, ActionType.None, "", true);
+            //session.Menu.Items.Add("LTContacts", content, ActionType.None, "", true);
 
             //session.Framework.CurrentUser.MyRecord
 
@@ -28,6 +28,7 @@ namespace LTISAct
             //}
 
             session.NavBar.AddCustomNavBarItemProvider(new CustomNavBarItemProvider(this.ServeCustomTodayNavBarItem));
+
         }
 
         private bool ServeCustomTodayNavBarItem(ACTSessionManager session, out string id, out string action, out string cssclass, out string imageUrl, out string width, out string height, out string caption)
@@ -46,7 +47,7 @@ namespace LTISAct
             width = "16";
             height = "16";
             //the caption on the item
-            caption = "Web Inquries";
+            caption = @"Web Inquries <span id=""LTContactCount"" style=""color: red; font-size: 10pt; font-weight: bold;""></span>";
             //return true to tell the provider to process this info and generate the custom navigation bar item
             return true;
         }
